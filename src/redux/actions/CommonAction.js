83040656fa0fbe7454994c828c5c9d84d57096d1
Cuong Nguyen Cao <nguyencaocuong.1996@gmail.com>
2017-12-11 15:08:48 +0700

    Init redux commonaction

---
+++ src/redux/actions/CommonAction.js
@@ -0,0 +1,18 @@
+
+const snippets = {
+    login: 'LOGIN',
+    logout: 'LOGOUT'
+};
+
+export default {
+    login: ({id, token, name, email})=> ({
+        type: snippets.login,
+        id: id,
+        token: token,
+        name: name,
+        email: email
+    }),
+    logout: ()=>({
+        type: snippets.logout
+    })
+}
