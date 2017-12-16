6cc6ab7c88d68fc1b3d7bcbedb3ac51a0b6cb2d1
Cuong Nguyen Cao <nguyencaocuong.1996@gmail.com>
2017-12-17 02:36:46 +0700

    Export screen

---
+++ src/screens/index.js
@@ -0,0 +1,34 @@
+import Tutorial from './Tutorial';
+
+import Login from './Login';
+import Register from './Register';
+
+import Home from './Home';
+
+import AddHotel from "./AddHotel";
+import HotelList from "./HotelList";
+import HotelMap from "./HotelMap";
+import HotelDetail from "./HotelDetail";
+
+import AddRestaurant from './AddRestaurant';
+import RestaurantList from './RestaurantList';
+import RestaurantMap from './RestaurantMap';
+import RestaurantDetail from './RestaurantDetail';
+
+
+
+
+module.exports = {
+    Tutorial,
+    Login,
+    Register,
+    Home,
+    AddHotel,
+    HotelList,
+    HotelMap,
+    HotelDetail,
+    AddRestaurant,
+    RestaurantList,
+    RestaurantMap,
+    RestaurantDetail
+};
